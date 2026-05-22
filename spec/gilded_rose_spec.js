0a93992e53d49b3184c6da7de23a44258097884e
malenajeitler <malenajeitler@gmail.com>
2026-05-22 13:35:27 +0200

    added TC_1

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,5 +1,12 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
+  // TC_1
+  it("should decrease sellIn and quality by 1 for normal items", function() {
+    const gildedRose = new Shop([ new Item("Normal Item", 10, 20) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(9);
+    expect(items[0].quality).toEqual(19);
+  });
 
 });
